7b307248c44160a825f3108f8ae7ebaceafee199
mikaelaliwanag <mikaela301@gmail.com>
2020-07-04 20:58:15 -0700

    Create post route that deletes checked items

--- app.js
+++ app.js
@@ -68,6 +68,19 @@ app.post("/", function(req, res){
   res.redirect("/");
 });
 
+app.post("/delete", function(req, res) {
+  
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err) {
+    if (!err) {
+      console.log("Successfully deleted item");
+      res.redirect("/");
+    }
+
+  })
+    
+});
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
