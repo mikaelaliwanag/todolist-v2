f5a330a3698d4c3002ef1ed184be5fbad65595db
mikaelaliwanag <mikaela301@gmail.com>
2020-07-04 19:29:56 -0700

    Create default items and saved them to DB

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true }, {useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
@@ -16,8 +16,31 @@ const itemsSchema = {
 
 const Item = mongoose.model("Item", itemsSchema);
 
+const item1 = new Item ({
+    name: "Type a new item below"
+  });
+
+const item2 = new Item ({
+    name: "Click the + button to add the new item"
+  });
+
+const item3 = new Item ({
+    name: "<--Click this to delete an item"
+  });
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err) {
+  if (err) {
+    console.log(err); 
+  } else {
+    console.log("Successfully saved default items to DB");
+  }
+});
+
 app.get("/", function(req, res) {
 
+  
   res.render("list", {listTitle: "Today", newListItems: items});
 
 });
